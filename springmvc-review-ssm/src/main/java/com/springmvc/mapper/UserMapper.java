70198bd810308a4410e382d1e99f159da00c53a6
Neal-Wangxd <wangxd_work@163.com>
2018-04-27 19:50:26 +0800

    USER mapper接口类

---
+++ springmvc-review-ssm/src/main/java/com/springmvc/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.springmvc.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springmvc.entity.User;
+
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年4月26日 下午4:53:21 
+* 类说明 
+*/
+@Mapper
+public interface UserMapper {
+	
+	public List<User> findAll();
+	
+	public User findOne(long id);
+	
+	@Transactional(propagation=Propagation.REQUIRED)
+	public boolean addUser(User user);
+
+	public boolean updateUser(User user);
+	
+	public boolean removeUser(long id);
+}
+ 
